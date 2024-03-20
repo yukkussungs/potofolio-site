06d4f291063aeb6389fc3776a17c1d2f83d19c72
ssungs <sws7420@gmail.com>
2024-03-20 16:23:42 +0900

    text edit

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -48,7 +48,7 @@ const AboutSection = () => {
   return (
     <section id="about" className="text-white">
         <div class="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16">
-          <Image src="/images/computer.jpg" width={500} height={500} />
+          <Image src="/images/computer.jpg" width={500} height={500} alt="computer" />
           <div className="mt-4">
             <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
             <p className="text-base md:text-lg">
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
         <div className="grid grid-cols-1 sm:grid-cols-12">
           <div className="col-span-7 place-self-center text-center sm:text-left">
             <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello, I'm {" "}</span> 
+              <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Nice to meet you, I'm {" "}</span> 
               <br/>
               <TypeAnimation
                 sequence={[
@@ -17,6 +17,8 @@ const HeroSection = () => {
                   'Woosung',
                   1000, // wait 1s before replacing "Mice" with "Hamsters"
                   'Web Developer',
+                  1000, // wait 1s before replacing "Mice" with "Hamsters"
+                  'Mobile Developer',
                   1000
                 ]}
                 wrapper="span"
@@ -24,7 +26,6 @@ const HeroSection = () => {
                 repeat={Infinity}
               />
             </h1>
-            <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
             <div>
               {/* <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">Hire Me</button> */}
               <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
